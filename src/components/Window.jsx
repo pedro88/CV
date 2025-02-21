ec6f4fe1ae0888f44959bb11c97e2557b1aafd97
pedro88 <sandrep_85@hotmail.com>
2025-02-21 14:34:43 +0100

    rebuild with tailwindcss

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -8,18 +8,20 @@ const Window = ({type}) => {
 
   return (
     <>
-    <div className='display'>
+    <div className='flex flex-col justify-center items-center'>
+    {/* <div className='display'> */}
 
-
-    <div className='window'>
-        <div className='window-name'>
-            <h2 className='name'>{type}</h2>
-            <div className='btn-container'>
-                <div className='icon'>
-                    <div className='reduce'></div>
+    <div className='grid grid-cols-3 grid-rows-[auto auto] border-solid border-3 w-[80vw] shadow-[10px_8px_0px_#13505B] m-5'>
+      {/* <div className='window'>  */}
+        <div className='border-solid border-b-3 bg-[#119DA4] flex flex-row justify-between item-center col-start-1 col-end-4'>
+       {/* <div className='window-name'> */}
+            <h2 className='ml-5'>{type}</h2>
+            <div className='flex flex-row mr-5'>
+                <div className='w-[20px] h-[20px] border-solid border-2 m-1 rounded-md flex flex-row justify-center item-center'>
+                    <div className='w-[15px] h-[15px] bg-contain '></div>
                 </div>
-                <div className='icon'>
-                    <div className='close'></div>
+                <div className='w-[20px] h-[20px] border-solid border-2 m-1 rounded-md flex flex-row justify-center item-center'>
+                    <div className='w-[15px] h-[15px] bg-contain bg-[url(../public/ressources/icons/g1.png)]'></div>
                 </div>
             </div>
         </div>
--- src/components/WindowContent.jsx
+++ src/components/WindowContent.jsx
@@ -10,13 +10,13 @@ const WindowContent = ({ type }) => {
 			case "coordonnate":
 				return (
 					<>
-                        <div className="column-1">
+                        <div className="bg-[url(../public/ressources/photo/DSC_6335.webp)] bg-cover bg-center col-start-1 col-end-2 ">
                         </div>
-						<div className="column-2">
+						<div className="col-start-2 col-end-3 mt-5 mb-5 pl-5">
 							<div>
 								<h2>Coordonnates</h2>
 							</div>
-							<div className="coordonnate-item">
+							<div className="flex">
 								<img src={data.coordonnate.nameLogo} alt="" />
 								<h3>name </h3>
 								<h3>
@@ -24,26 +24,26 @@ const WindowContent = ({ type }) => {
 									{data.coordonnate.lastName}
 								</h3>
 							</div>
-							<div className="coordonnate-item">
+							<div className="flex">
 								<img src={data.coordonnate.GSMLogo} alt="" />
 								<h3>GSM </h3>
 								<h3>
                                 {" : "}{data.coordonnate.GSM}</h3>
 							</div>
-							<div className="coordonnate-item">
+							<div className="flex">
 								<img src={data.coordonnate.emailLogo} alt="" />
 								<h3>email </h3>
 								<h3>
                                 {" : "}{data.coordonnate.email}</h3>
 							</div>
-							<div className="coordonnate-item">
+							<div className="flex">
 								<img src={data.coordonnate.adressLogo} alt="" />
 								<h3>address </h3>
 								<h3>
                                 {" : "}{data.coordonnate.address}</h3>
 							</div>
 						</div>
-						<div className="column-3">
+						<div className="col-start-3 col-end-4 border-l-3 border-dashed mt-5 mb-5 pl-5 ">
 							<div>
 								<h2>Interest</h2>
 							</div>
