c003ce4c6eab8d769c993340cc40e00fe6c8f57e
pedro88 <sandrep_85@hotmail.com>
2025-02-22 13:29:52 +0100

    picture path issue resolving

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -19,14 +19,14 @@ const Menu = () => {
 
         </div>
     <div className={`flex flex-col items-center ${menu ? "block" : "hidden"}`}>
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./ressources/function.png" alt="" />
-        <img className=' h-13 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./ressources/profile.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./ressources/experiences.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./ressources/formation.png" alt="" />
-        <img className=' h-15 w-14 mt-3 hover:scale-105 duration-100 ease-out' src="./ressources/skill.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./ressources/pdf.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./ressources/printer.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./ressources/write.png" alt="" />
+        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="/function.png" alt="" />
+        <img className=' h-13 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="/profile.png" alt="" />
+        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="/experiences.png" alt="" />
+        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="/formation.png" alt="" />
+        <img className=' h-15 w-14 mt-3 hover:scale-105 duration-100 ease-out' src="/skill.png" alt="" />
+        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="/pdf.png" alt="" />
+        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="/printer.png" alt="" />
+        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="/write.png" alt="" />
         <img className=' h-10 mt-3' src="./public/ressources/menu/menu-half-border.png" alt=""/>
 
     </div>
