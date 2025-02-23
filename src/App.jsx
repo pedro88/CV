4d725e9d22ffd9ec34d2e38f65b8f567c12a64fc
pedro88 <sandrep_85@hotmail.com>
2025-02-23 07:58:01 +0100

    night work

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,75 @@ import Menu from "./components/Menu";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [displayExperience, setDisplayExperience] = useState(true)
+	const [displayFormation, setDisplayFormation] = useState(true)
+	const [displayFunction, setDisplayFunction] = useState(true)
+	const [displayPDF, setDisplayPDF] = useState(true)
+	const [displayPrinter, setDisplayPrinter] = useState(true)
+	const [displayProfile, setDisplayProfile] = useState(true)
+	const [displaySkill, setDisplaySkill] = useState(true)
+	const [displayWrite, setDisplayWrite] = useState(true)
+
+const handleDisplayExperience = () => {
+	setDisplayExperience((prev) => !prev)
+}
+const handleDisplayFormation = () => {
+	setDisplayFormation((prev) => !prev)
+}
+const handleDisplayFunction = () => {
+	setDisplayFunction((prev) => !prev)
+}
+const handleDisplayPDF = () => {
+	setDisplayPDF((prev) => !prev)
+}
+const handleDisplayPrinter = () => {
+	setDisplayPrinter((prev) => !prev)
+}
+const handleDisplayProfile = () => {
+	setDisplayProfile((prev) => !prev)
+}
+const handleDisplaySkill = () => {
+	setDisplaySkill((prev) => !prev)
+}
+const handleDisplayWrite = () => {
+	setDisplayWrite((prev) => !prev)
+}
+const [close, setClose] = useState(true)
+
 
 	return (
 		<div >
-			<Menu/>
+			<Menu
+				handleDisplayExperience={handleDisplayExperience}
+				handleDisplayFormation={handleDisplayFormation}
+				handleDisplayFunction={handleDisplayFunction}
+				handleDisplayPDF={handleDisplayPDF}
+				handleDisplayPrinter={handleDisplayPrinter}
+				handleDisplayProfile={handleDisplayProfile}
+				handleDisplaySkill={handleDisplaySkill}
+				handleDisplayWrite={handleDisplayWrite}
+			/>
 			<div className="flex flex-col justify-center items-center">
-			<Window type={"function"} />
-			<Window type={"coordonnate"} />
+				<div className={`${displayFunction ? "block" : "hidden"}`}>
+				<Window type={"function"} />
+				</div>
+				<div className={`${displayProfile ? "block" : "hidden"}`}>
+				<Window type={"coordonnate"} />
+				</div>
 			</div>
 			<div className="flex flex-row justify-center items-start">
-			<Window type={"experiences"}/>
-			<div className="flex flex-col">
-			<Window type={"formations"}/>
-			<Window type={"competences"}/>
-			</div>
+				<div className={`${displayExperience ? "block" : "hidden"}`}>
+				<Window type={"experiences"} />
+				</div>
+				<div className="flex flex-col">
+					<div className={`${displayFormation ? "block" : "hidden"}`}>
+					<Window type={"formations"} />
+					</div>
+					<div className={`${displaySkill ? "block" : "hidden"}`}>
+					<Window type={"competences"} />
+					</div>
+
+				</div>
 
 			</div>
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react'
 
-const Menu = () => {
+const Menu = ({ handleDisplayExperience, handleDisplayFormation, handleDisplayFunction, handleDisplayPDF, handleDisplayPrinter, handleDisplayProfile, handleDisplaySkill, handleDisplayWrite}) => {
     const [menu, setMenu] = useState(false)
 
     const handleMenu = () => {
         setMenu((prev) => !prev)
-
     }
-
-
   return (
     <>
     <div className='absolute h-20 ml-10 mt-5'>
@@ -19,14 +16,14 @@ const Menu = () => {
 
         </div>
     <div className={`flex flex-col items-center ${menu ? "block" : "hidden"}`}>
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./function.png" alt="" />
-        <img className=' h-13 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./profile.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./experiences.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./formation.png" alt="" />
-        <img className=' h-15 w-14 mt-3 hover:scale-105 duration-100 ease-out' src="./skill.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./pdf.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./printer.png" alt="" />
-        <img className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./write.png" alt="" />
+        <img onClick={handleDisplayFunction} className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./function.png" alt="" />
+        <img onClick={handleDisplayProfile} className=' h-13 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./profile.png" alt="" />
+        <img onClick={handleDisplayExperience} className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./experiences.png" alt="" />
+        <img onClick={handleDisplayFormation} className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./formation.png" alt="" />
+        <img onClick={handleDisplaySkill} className=' h-15 w-14 mt-3 hover:scale-105 duration-100 ease-out' src="./skill.png" alt="" />
+        <img onClick={handleDisplayPDF} className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./pdf.png" alt="" />
+        <img onClick={handleDisplayPrinter} className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./printer.png" alt="" />
+        <img onClick={handleDisplayWrite} className=' h-15 w-15 mt-3 hover:scale-105 duration-100 ease-out' src="./write.png" alt="" />
         <img className=' h-10 mt-3' src="./menu-half-border.png" alt=""/>
 
     </div>
